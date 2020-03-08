0bf709665d74442dcf625cc33ada02a9a9a2402e
Przemysław Piasecki <253089@student.pwr.edu.pl>
2020-03-08 05:01:27 +0100

    improved readability

--- Projekt_1/solutions/przemoCBE/minesweeperboard.hpp
+++ Projekt_1/solutions/przemoCBE/minesweeperboard.hpp
@@ -4,14 +4,15 @@
 #include "field.hpp"
 
 class minesweeperBoard{
-	field board[100][100];
-	unsigned int width;
-	unsigned int height;
-	std::string fieldDebug(const field &pole) const;	//for debugDisplay()
- public:
-	minesweeperBoard(unsigned int w,unsigned int h,unsigned int mineAmount);
-	minesweeperBoard();
-	void debugDisplay() const;
+	public:
+		minesweeperBoard(unsigned int w,unsigned int h,unsigned int mineAmount);
+		minesweeperBoard();
+		void debugDisplay() const;
+	private:
+		field board[100][100];
+		unsigned int width;
+		unsigned int height;
+		std::string fieldDebug(const field &pole) const;	//for debugDisplay()
 };
 
 #endif
